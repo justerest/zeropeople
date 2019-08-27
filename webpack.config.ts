c1b1fd8083861f9fc88a95dbadb1cca8b31c28a9
justerest <justerest@gmailcom>
2019-08-27 23:58:01 +0500

    feat: extract css

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,32 @@
-import path from 'path';
+import { resolve } from 'path';
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
 const config: Configuration = {
   mode: 'production',
   entry: ['./src/index.css', './src/index.tsx'],
-  output: { path: path.resolve(__dirname, 'dist'), filename: 'main.js' },
+  output: { path: resolve(__dirname, 'dist'), filename: 'main.js' },
   resolve: { extensions: ['.ts', '.tsx', '.js'] },
   module: {
     rules: [
       { test: /\.ts(x?)$/, exclude: /node_modules/, use: [{ loader: 'ts-loader' }] },
       { test: /\.(html|svg)$/, use: [{ loader: 'html-loader', options: { interpolate: true } }] },
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-      { test: /\.(png|jpg|gif)$/, loader: 'file-loader', options: { name: '[name].[ext]?[hash]', outputPath: 'img/' } },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      { test: /\.(png|jpg|gif)$/, loader: 'file-loader', options: { name: '[name].[ext]?[hash]' } },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: 'src/index.html',
       inject: false,
-      minify: { removeComments: true, collapseWhitespace: true, removeAttributeQuotes: true },
+      minify: { removeComments: true, collapseWhitespace: true },
     }),
+    new MiniCssExtractPlugin(),
+    new OptimizeCssAssetsPlugin(),
   ],
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
+  externals: { react: 'React', 'react-dom': 'ReactDOM' },
 };
 
 export default config;
